8d0afe7d6fd876c2e149694ef86b0942461ce82f
Alex-rns <alexrns3@gmail.com>
2018-10-30 19:44:19 +0200

    Lesson3: Create registration.php

---
+++ app/code/BelodubrovskyiAn/MyCustomModule/registration.php
@@ -0,0 +1,7 @@
+
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'BelodubrovskyiAn_MyCustomModule',
+    __DIR__
+);
