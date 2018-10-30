693dd658e6fa86959df1fb3d37bfb2039cf8b75b
Alex-rns <alexrns3@gmail.com>
2018-10-30 19:37:11 +0200

    Add files via upload
    Add env.php

---
+++ app/etc/env.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'c20271e2a7fec7bd6effab6646aa3330'
+    ],
+    'db' => [
+        'table_prefix' => 'm2_',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento_226',
+                'username' => 'magento_226',
+                'password' => 'magento_226',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'compiled_config' => 1
+    ],
+    'install' => [
+        'date' => 'Tue, 30 Oct 2018 13:32:16 +0000'
+    ]
+];
