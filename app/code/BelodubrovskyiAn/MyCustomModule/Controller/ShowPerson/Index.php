c8eea1af5369601a715bebe914d0bf5e9c56ae14
Alex-rns <alexrns3@gmail.com>
2018-11-04 14:18:16 +0200

    Lesson 3-new: Create custom Controller - ShowPerson with test "die"

---
+++ app/code/BelodubrovskyiAn/MyCustomModule/Controller/ShowPerson/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace BelodubrovskyiAn\MyCustomModule\Controller\ShowPerson;
+use Magento\Framework\App\Action\Context;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     */
+    public function execute()
+    {
+        die("Hello 😉 - BelodubrovskyiAn\\MyCustomModule\\Controller\\ShowPerson\\Index - execute() method");
+    }
+}
