71a7824b6ad045905868e821a25c21ebc4bd59d7
Alex-rns <alexrns3@gmail.com>
2018-11-21 17:10:03 +0200

    Lesson 6: Add custom validation to the 'phone' field to ensure it is a ukrainian mobile phone.

--- app/code/BelodubrovskyiAn/AskQuestion/view/frontend/requirejs-config.js
+++ app/code/BelodubrovskyiAn/AskQuestion/view/frontend/requirejs-config.js
@@ -4,5 +4,13 @@ var config = {
             askQuestionSample: 'BelodubrovskyiAn_AskQuestion/js/ask-question-sample',
             geekhubValidationAlert: 'BelodubrovskyiAn_AskQuestion/js/validation-alert'
         }
+    },
+    config: {
+        mixins: {
+            'mage/validation': {
+                'BelodubrovskyiAn_AskQuestion/js/validation-ua-phone-nubmer-mixin': true
+            }
+        }
     }
+
 };
