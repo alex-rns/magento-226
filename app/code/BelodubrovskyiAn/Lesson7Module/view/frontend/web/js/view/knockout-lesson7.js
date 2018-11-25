94e3683d6ef9e6aa6e61cec41048a3e012b0d722
Alex-rns <alexrns3@gmail.com>
2018-11-25 15:18:06 +0200

    Lesson 7: Create a timer loop within component which updates the value every second.

--- app/code/BelodubrovskyiAn/Lesson7Module/view/frontend/web/js/view/knockout-lesson7.js
+++ app/code/BelodubrovskyiAn/Lesson7Module/view/frontend/web/js/view/knockout-lesson7.js
@@ -7,14 +7,28 @@ define(
     function ($, Component, ko) {
         'use strict';
 
+        let self;
+
         return Component.extend({
             myTimer: ko.observable(0),
-
             /**
-             * Template function
+             * Initialize function
              */
             initialize: function () {
+                self = this;
                 this._super();
+                //call the incrementTime function to run on intialize
+                this.incrementTime();
+            },
+            /**
+             * Increment myTimer every second
+             */
+            incrementTime: function () {
+                let t = 0;
+                setInterval(function () {
+                    t++;
+                    self.myTimer(t);
+                }, 1000);
             }
         });
     }
