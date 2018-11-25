d872d3e0b130bca99c048b2cc5c55bdcbfb06afe
Alex-rns <alexrns3@gmail.com>
2018-11-25 16:29:03 +0200

    Lesson 7: Split out the RGB values into a simple storage model.

--- app/code/BelodubrovskyiAn/Lesson7Module/view/frontend/web/js/view/knockout-lesson7.js
+++ app/code/BelodubrovskyiAn/Lesson7Module/view/frontend/web/js/view/knockout-lesson7.js
@@ -2,18 +2,23 @@ define(
     [
         'jquery',
         'uiComponent',
-        'ko'
+        'ko',
+        'BelodubrovskyiAn_Lesson7Module/js/model/rgb-model'
     ],
-    function ($, Component, ko) {
+    function ($, Component, ko, rgbModel) {
         'use strict';
 
         let self;
 
         return Component.extend({
             myTimer: ko.observable(0),
-            red: ko.observable(0),
-            blue: ko.observable(0),
-            green: ko.observable(0),
+            /**
+             * Computed random color from rgbModel
+             */
+            randomColour: ko.computed(function () {
+                //we are using the aliased rgbModel here giving us access to the RGB values
+                return 'rgb(' + rgbModel.red() + ', ' + rgbModel.blue() + ', ' + rgbModel.green() + ')';
+            }, this),
             /**
              * Initialize function
              */
@@ -23,10 +28,6 @@ define(
                 //call the incrementTime function to run on intialize
                 this.incrementTime();
                 this.subscribeToTime();
-                this.randomColour = ko.computed(function () {
-                    //return the random colour value
-                    return 'rgb(' + this.red() + ', ' + this.blue() + ', ' + this.green() + ')';
-                }, this);
             },
             /**
              * Increment myTimer every second
@@ -42,25 +43,9 @@ define(
              * Subscribe to observable myTimer
              */
             subscribeToTime: function () {
-                this.myTimer.subscribe(function (newValue) {
-                    console.log(newValue);
-                    self.updateTimerTextColour();
+                this.myTimer.subscribe(function () {
+                    rgbModel.updateColour();
                 });
-            },
-            /**
-             * Generating a random number
-             */
-            randomNumber: function () {
-                return Math.floor((Math.random() * 255) + 1);
-            },
-            /**
-             * Updating color depending on the generated number
-             */
-            updateTimerTextColour: function () {
-                //define RGB values
-                this.red(self.randomNumber());
-                this.blue(self.randomNumber());
-                this.green(self.randomNumber());
             }
         });
     }
