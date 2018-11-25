bc88e60163426f6aa1c191c23f070bd66f999c8e
Alex-rns <alexrns3@gmail.com>
2018-11-25 15:48:36 +0200

    Lesson 7: Subscribe to observable myTimer. Every time the myTimer variable updates, update the colour of the timer text to a random colourEvery time the myTimer variable updates, changed the colour of the timer text to a random colour.

--- app/code/BelodubrovskyiAn/Lesson7Module/view/frontend/web/js/view/knockout-lesson7.js
+++ app/code/BelodubrovskyiAn/Lesson7Module/view/frontend/web/js/view/knockout-lesson7.js
@@ -11,6 +11,7 @@ define(
 
         return Component.extend({
             myTimer: ko.observable(0),
+            randomColour: ko.observable('rgb(0, 0, 0)'),
             /**
              * Initialize function
              */
@@ -19,6 +20,7 @@ define(
                 this._super();
                 //call the incrementTime function to run on intialize
                 this.incrementTime();
+                this.subscribeToTime();
             },
             /**
              * Increment myTimer every second
@@ -29,6 +31,31 @@ define(
                     t++;
                     self.myTimer(t);
                 }, 1000);
+            },
+            /**
+             * Subscribe to observable myTimer
+             */
+            subscribeToTime: function () {
+                this.myTimer.subscribe(function (newValue) {
+                    console.log(newValue);
+                    self.updateTimerTextColour();
+                });
+            },
+            /**
+             * Generating a random number
+             */
+            randomNumber: function () {
+                return Math.floor((Math.random() * 255) + 1);
+            },
+            /**
+             * Generating a random colour
+             */
+            updateTimerTextColour: function () {
+                //define RGB values
+                let red = self.randomNumber(),
+                    blue = self.randomNumber(),
+                    green = self.randomNumber();
+                self.randomColour('rgb(' + red + ', ' + blue + ', ' + green + ')');
             }
         });
     }
