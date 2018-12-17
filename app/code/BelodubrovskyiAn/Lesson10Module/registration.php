4d9b293e3a21c2f473ec43124cc240e11de125ff
Alex-rns <alexrns3@gmail.com>
2018-12-17 12:53:26 +0200

    Lesson 10: Create new module.

---
+++ app/code/BelodubrovskyiAn/Lesson10Module/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'BelodubrovskyiAn_Lesson10Module',
+    __DIR__
+);
