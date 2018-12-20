7362557c4c7f2dbbb4d49ced7b6e4bc1f71aff44
Alex-rns <alexrns3@gmail.com>
2018-12-20 15:59:54 +0200

    Lesson10: Edit product form. Add custom field.

---
+++ app/code/BelodubrovskyiAn/Lesson10Module/Ui/DataProvider/Product/Form/Modifier/NewField.php
@@ -0,0 +1,77 @@
+<?php
+namespace BelodubrovskyiAn\Lesson10Module\Ui\DataProvider\Product\Form\Modifier;
+use Magento\Catalog\Model\Locator\LocatorInterface;
+use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\AbstractModifier;
+use Magento\Ui\Component\Form\Fieldset;
+use Magento\Ui\Component\Form\Field;
+use Magento\Ui\Component\Form\Element\Select;
+use Magento\Ui\Component\Form\Element\DataType\Text;
+class NewField extends AbstractModifier
+{
+    private $locator;
+    public function __construct(
+        LocatorInterface $locator
+    ) {
+        $this->locator = $locator;
+    }
+    public function modifyData(array $data)
+    {
+        $productId = $this->locator->getProduct()->getId();
+        $data = array_replace_recursive(
+            $data,
+            [
+                $productId => [
+                    'product' => [
+                        'enabled' => 'your_value'
+                    ]
+                ]
+            ]
+        );
+
+        return $data;
+    }
+    public function modifyMeta(array $meta)
+    {
+        $meta = array_replace_recursive(
+            $meta,
+            [
+                'custom_fieldset' => [
+                    'arguments' => [
+                        'data' => [
+                            'config' => [
+                                'label' => __('Custom Fieldset'),
+                                'componentType' => Fieldset::NAME,
+                                'dataScope' => 'data.product.custom_fieldset',
+                                'collapsible' => true,
+                                'sortOrder' => 5,
+                            ],
+                        ],
+                    ],
+                    'children' => [
+                        'custom_field' => $this->getCustomField()
+                        ],
+                ]
+            ]
+        );
+        return $meta;
+    }
+
+    public function getCustomField()
+    {
+        return [
+            'arguments' => [
+                'data' => [
+                    'config' => [
+                        'label' => __('Custom Field'),
+                        'componentType' => Field::NAME,
+                        'formElement' => \Magento\Ui\Component\Form\Element\Input::NAME,
+                        'dataScope' => 'enabled',
+                        'dataType' => Text::NAME,
+                        'sortOrder' => 10,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
+
