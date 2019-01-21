2d8b200480147d9ff8c91394ec62fa9eacb9028a
Alex-rns <alexrns3@gmail.com>
2019-01-21 21:19:44 +0200

    Lesson 11: Fix after homework check: injecting \Magento\Framework\Filesystem\DirectoryList $dir

--- app/code/BelodubrovskyiAn/Lesson11ModuleOop/Model/FileList.php
+++ app/code/BelodubrovskyiAn/Lesson11ModuleOop/Model/FileList.php
@@ -8,11 +8,26 @@ namespace BelodubrovskyiAn\Lesson11ModuleOop\Model;
 class FileList
 {
     /**
+     * @var \Magento\Framework\Filesystem\DirectoryList
+     */
+    protected $_dir;
+
+    /**
+     * FileList constructor.
+     * @param \Magento\Framework\Filesystem\DirectoryList $dir
+     */
+    public function __construct(\Magento\Framework\Filesystem\DirectoryList $dir)
+    {
+        $this->_dir = $dir;
+    }
+
+    /**
      * @return \RecursiveIteratorIterator
      */
     public function giveFileList()
     {
-        $ite = new \RecursiveDirectoryIterator('/misc/apps/magento-226/app/code/BelodubrovskyiAn/Lesson11ModuleOop/');
+        $mypath = $this->_dir->getRoot() . '/app/code/';
+        $ite = new \RecursiveDirectoryIterator($mypath);
         $fileList = new \RecursiveIteratorIterator($ite);
         return $fileList;
     }
