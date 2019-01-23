ff7254dac182b1827ae3c46ffce10dcc7fda0cf4
Alex-rns <alexrns3@gmail.com>
2019-01-23 15:54:28 +0200

    Lesson 12: Added InstallSchema and UpgradeSchema for "AskQuestion" module.

---
+++ app/code/BelodubrovskyiAn/AskQuestion/Model/AskQuestion.php
@@ -0,0 +1,12 @@
+<?php
+namespace BelodubrovskyiAn\AskQuestion\Model;
+
+/**
+ * Class AskQuestion
+ * @package BelodubrovskyiAn\AskQuestion\Model
+ */
+class AskQuestion
+{
+    const STATUS_PENDING = 'pending';
+    const STATUS_PROCESSED = 'processed';
+}
---
+++ app/code/BelodubrovskyiAn/AskQuestion/Setup/InstallSchema.php
@@ -0,0 +1,20 @@
+<?php
+namespace BelodubrovskyiAn\AskQuestion\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+/**
+ * Class InstallSchema
+ * @package BelodubrovskyiAn\AskQuestion\Setup
+ */
+class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
+{
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+    }
+}
