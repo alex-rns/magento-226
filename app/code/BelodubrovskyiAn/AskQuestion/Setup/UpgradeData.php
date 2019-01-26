0608e636eb587ab5a571cda1befe6c4ad9ad3a63
Alex-rns <alexrns3@gmail.com>
2019-01-26 22:24:18 +0200

    Lesson 12: Some check.

--- app/code/BelodubrovskyiAn/AskQuestion/Setup/UpgradeData.php
+++ app/code/BelodubrovskyiAn/AskQuestion/Setup/UpgradeData.php
@@ -30,7 +30,7 @@ class UpgradeData implements UpgradeDataInterface
      */
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        if (version_compare($context->getVersion(), '1.0.2', '<')) {
+        if (version_compare($context->getVersion(), '1.0.3', '<')) {
             $statuses = [AskQuestion::STATUS_PENDING, AskQuestion::STATUS_PROCESSED];
             for ($i = 1; $i <= 5; $i++) {
                 /** @var AskQuestion $askQuestion */
