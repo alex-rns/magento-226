463dc53b7cc62e745ac3e167a638eec0adeb4dfd
Alex-rns <alexrns3@gmail.com>
2019-02-08 22:37:46 +0200

    Lesson 14: Create console command that accepts Product ID and Qty as arguments and updated product qty.

--- app/code/BelodubrovskyiAn/AskQuestion/Console/Command/UpdateProductQty.php
+++ app/code/BelodubrovskyiAn/AskQuestion/Console/Command/UpdateProductQty.php
@@ -5,9 +5,43 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 
+/**
+ * Class UpdateProductQty
+ * @package BelodubrovskyiAn\AskQuestion\Console\Command
+ */
 class UpdateProductQty extends \Symfony\Component\Console\Command\Command
 {
-    const DEFAULT_COUNT = 20;
+    /**
+     * @var \Magento\Framework\App\State
+     */
+    protected $state;
+
+    /**
+     * @var \Magento\CatalogInventory\Api\StockRegistryInterface
+     */
+    protected $stockRegistry;
+
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    protected $productRepository;
+
+    /**
+     * UpdateProductQty constructor.
+     * @param \Magento\Framework\App\State $state
+     * @param \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry
+     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+     */
+    public function __construct(
+        \Magento\Framework\App\State $state,
+        \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+    ) {
+        $this->state = $state;
+        $this->stockRegistry = $stockRegistry;
+        $this->productRepository = $productRepository;
+        parent::__construct();
+    }
 
     protected function configure()
     {
@@ -15,21 +49,50 @@ class UpdateProductQty extends \Symfony\Component\Console\Command\Command
             ->setDescription('Update product Qty')
             ->setDefinition([
                 new InputArgument(
-                    'count',
+                    'product_id',
                     InputArgument::OPTIONAL,
-                    'Count'
+                    'Product ID'
+                ),
+                new InputArgument(
+                    'qty',
+                    InputArgument::OPTIONAL,
+                    'Qty'
                 )
             ]);
         parent::configure();
     }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|void|null
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $count = $input->getArgument('count') ?: self::DEFAULT_COUNT;
-        $i = 0;
-        while ($i < $count) {
-            ++$i;
-            $output->writeln("<info>Generated item #$i...<info>");
+        try {
+            $this->state->setAreaCode(\Magento\Framework\App\Area::AREA_ADMINHTML);
+
+            $productId = (int) $input->getArgument('product_id');
+
+            $product = $this->productRepository->getById($productId);
+
+            $stockItem = $this->stockRegistry->getStockItem($productId);
+
+            $stockItem->setQty((int) $input->getArgument('qty'));
+
+            $stockItem->setIsInStock((int) $input->getArgument('qty'));
+
+            $newQty = (int) $input->getArgument('qty');
+
+            $this->stockRegistry->updateStockItemBySku($product->getSku(), $stockItem);
+
+            $productName = $product->getName();
+
+            $output->writeln('<info>Updated!<info>' . " New quantity of $productName is $newQty");
+        } catch (\Exception $e) {
+            $output->writeln("<error>{$e->getMessage()}<error>");
         }
-        $output->writeln("<info>Completed!<info>");
     }
 }
