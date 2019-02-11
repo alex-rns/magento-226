795a91170fd9aea8095233e2813984d51e15ce13
Alex-rns <alexrns3@gmail.com>
2019-02-11 20:40:06 +0200

    Lesson 14: Created a cron job to change "Question" status.

--- app/code/BelodubrovskyiAn/AskQuestion/Cron/ChangeQuestionStatus.php
+++ app/code/BelodubrovskyiAn/AskQuestion/Cron/ChangeQuestionStatus.php
@@ -1,22 +1,60 @@
 <?php
 namespace BelodubrovskyiAn\AskQuestion\Cron;
 
+use BelodubrovskyiAn\AskQuestion\Model\AskQuestion;
+
 class ChangeQuestionStatus
 {
+    /**
+     * @var \BelodubrovskyiAn\AskQuestion\Model\ResourceModel\AskQuestion\CollectionFactory
+     */
+    private $collectionFactory;
     /**
      * @var \Psr\Log\LoggerInterface
      */
     private $logger;
+
     /**
-     * Example constructor.
+     * ChangeQuestionStatus constructor.
      * @param \Psr\Log\LoggerInterface $logger
+     * @param \BelodubrovskyiAn\AskQuestion\Model\ResourceModel\AskQuestion\CollectionFactory $collectionFactory
      */
-    public function __construct(\Psr\Log\LoggerInterface $logger)
-    {
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger,
+        \BelodubrovskyiAn\AskQuestion\Model\ResourceModel\AskQuestion\CollectionFactory $collectionFactory
+    ) {
         $this->logger = $logger;
+        $this->collectionFactory = $collectionFactory;
     }
-    public function execute()
+    public function execute() :void
     {
-        $this->logger->critical('Cron job is working!');
+        $collection = $this->collectionFactory->create();
+
+        $collection->addFieldToFilter('status', AskQuestion::STATUS_PENDING)
+                   ->addFieldToFilter('created_at', ['lt' => $this->getPointDateChange()]);
+        echo "All questions with lifetime more than {$this->getDaysNumber()} days changed status to Answered \n";
+
+        foreach ($collection as $item) {
+            $item->setStatus(AskQuestion::STATUS_PROCESSED)->save();
+        }
+        $this->logger->warning('Cron job ChangeQuestionStatus completed!');
+    }
+
+    /**
+     * @return false|string
+     */
+    private function getPointDateChange()
+    {
+        $currentDate = date('Y-m-d h:i:s');
+        $timeInterval = strtotime('-' . $this->getDaysNumber() . ' day', strtotime($currentDate));
+        return date('Y-m-d h:i:s', $timeInterval);
+    }
+
+    /**
+     * @return int
+     */
+    private function getDaysNumber()
+    {
+        return 3;
     }
 }
